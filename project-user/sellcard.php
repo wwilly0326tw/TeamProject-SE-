4d32a7f5dd031734a536c418bf7dda900003922f
tsai <kile107107@gmail.com>
2016-12-17 17:17:52 +0800

    sql存入時間

--- project-user/sellcard.php
+++ project-user/sellcard.php
@@ -7,7 +7,8 @@ $uid=$_SESSION['uID']; //player id
 $cardid=$_POST['cid']; //要賣的卡片id
 $num=$_POST['number']; //要賣的數量
 $price=$_POST['price']; //底價
-$deadline=$_POST['deadline']; //結標時間
+$dl=date("Y-m-d H:i:s", $_POST['deadline']); //結標時間
+$deadline=strtotime($dl);
 
 //select //要賣的那張卡片id
 $sql="select * from `cardbag` where `playerid`='$uid' and `cardid`=$cardid";
@@ -17,16 +18,17 @@ $number=(int)($res['count']);
 if ($number >= $num) { //擁有卡片>賣出
     $count = $number - $num; //扣掉賣掉的數量
 
-    //update倉庫card中數量
-    $sql2="update `cardbag` set `count`='$count' where `playerid`='$uid' and `cardid`='$cardid'";
-    mysqli_query($conn, $sql2) or die ("update count error");
-
     //insert到拍賣品
     $sql3="insert into `products`(cardid, sellerid, count, reserve_price, current_price, deadline) values($cardid, $uid, $num, $price, $price, $deadline)";
     mysqli_query($conn, $sql3) or die ("insert products error");
+    
+    //update倉庫card中數量
+    $sql2="update `cardbag` set `count`='$count' where `playerid`='$uid' and `cardid`='$cardid'";
+    mysqli_query($conn, $sql2) or die ("update count error");
 
     echo "已拍賣".$cardid.'卡片'.$num.'張';
 } else {
     echo "卡片不夠";
 }
+header("Refresh: 1; url=showInventory.php");
 ?>
