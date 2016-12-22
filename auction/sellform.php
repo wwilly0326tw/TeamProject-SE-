880eda1d0ea72cf10e7090f599253d8c9db0c747
tsai <kile107107@gmail.com>
2016-12-22 15:31:05 +0800

    sellform css

--- auction/sellform.php
+++ auction/sellform.php
@@ -2,30 +2,11 @@
 session_start();
 $uID=$_SESSION['playerID'];
 $cid=$_GET['cid'];
-
 ?>
+<!DOCTYPE html>
+<html>
 <head>
-    <style type="text/css">
-    .form {
-        width: 200px;
-    	margin: 0 auto;
-    }
-    .form input {
-        width: 90%;
-       	align: 'center';
-    }	
-    form {
-        display: table;
-    }
-    form p{
-        display: table-row;
-    }
-    form h1{
-        display: table-cell;
-        /*margin: 3px;*/
-    }
-    </style>
-     
+<title>Sell card</title>
     <link href='http://fonts.googleapis.com/css?family=Gloria+Hallelujah' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=PT+Mono' rel='stylesheet' type='text/css'>
     <!--icon-->
@@ -33,20 +14,29 @@ $cid=$_GET['cid'];
     <!--css-->
     <link href="css/test.css" rel="stylesheet" type="text/css"/>
 </head>
-<form class="form" action="php/sellcard.php" method="POST" >
-<input type="hidden" name="uID" value="$uID">
-<input type="hidden" name='cid' value='<?php echo $cid;?>'>
-<p><h1 align=center> <img width="200px" style="border:2px solid #99BBFF" src="img/<?php echo $cid;?>.jpg"> </h1></p>
-<p><h1 align=center> 數量<input type="number" name="number" value='1' min="1"></h1></p>
-<p><h1 align=center> 底價<input type="text" name="price" value='1' min="1"></h1></p>
-<p><h1 align=center> 結標時間<br>
-<select name="deadline" align="center">
-    <option value="1">一天</option>
-    <option value="2">兩天</option>
-    <option value="3">三天</option>
-    <option value="4">四天</option>
-    <option value="5">五天</option>
-</select>
-</h1></p>
-<p><h1 align=center><input type=submit value="賣出"></h1></p>
-</form>
+<body>
+<div id="content" style="margin-top:5%;">
+    <form class="form" action="php/sellcard.php" method="POST" >
+        <input type="hidden" name="uID" value="$uID">
+        <input type="hidden" name='cid' value='<?php echo $cid;?>'>
+        <div><h1 align=center> <img width="200px" style="border:2px solid #99BBFF" src="img/<?php echo $cid;?>.jpg"> </h1></div>
+        <div><h2><span class="selltext">Count</span><br/>
+            <input class="input" type="number" name="number" value='1' min="1"></h2>
+        </div>
+        <div><h2><span class="selltext">Reserve Price</span><br/>
+            <input class="input" type="text" name="price" value='1' min="1"></h2>
+        </div>
+        <div><h2 class="selltext">Deadline<br/>
+            <select id="opt" name="deadline" align="center">
+                <option value="1">one day</option>
+                <option value="2">two days</option>
+                <option value="3">three days</option>
+                <option value="4">four days</option>
+                <option value="5">five days</option>
+            </select></h2>
+        </div>
+       <div><h1 align=center><button class="btn" type="submit" value="sell">Sell</button></h1></div>
+    </form>
+</div>
+</body>
+</html>
